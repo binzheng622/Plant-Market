04303eac9b64c2b9e2bc04e7fecd17c9584803da
Bin Zheng <binzheng622@gmail.com>
2024-02-21 16:50:31 -0500

    nextjs change

--- client/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import plantsReducer from './reducers/plantsReducer';
-
-//redux store setup
-export const store = configureStore({
-  reducer: {
-    plants: plantsReducer,
-  },
-});
